1c968131ebd04a225da02e5625587e8a3b065066
tomoyane <tohito0430@gmail.com>
2019-09-14 10:16:59 +0900

    dep to mod

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/tomoyane/grant-n-z-cli
--- main.go
+++ main.go
@@ -1,20 +1,6 @@
 package main
 
-import (
-	"os"
-
-	"github.com/urfave/cli"
-)
-
-var (
-	app cli.App
-)
-
 func main() {
-	app := cli.NewApp()
-	app.Version = "1.0.0"
-	app.Usage = "make an explosive entrance"
-	app.Run(os.Args)
 }
 
 func setUri() {
